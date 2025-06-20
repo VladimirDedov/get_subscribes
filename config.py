48d44181192eafd18bc9e1b0ebaebcdf73b234e4
Vladimir Dedov <vldedov@bk.ru>
2025-06-20 17:34:07 +0500

    added config file for env

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+
+INVATE_LINK = os.getenv("INVATE_LINK")
+USERNAME = os.getenv("USERNAME")
+
+PHONE = os.getenv("PHONE")
+GROUP_ID = os.getenv("GROUP_ID")
